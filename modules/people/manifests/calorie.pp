32ef932c690c98f82f58bc7bc03be1f59fc5d928
YuuShigetani <s2g4t1n2@gmail.com>
2013-11-27 13:35:09 +0900

    update ruby version

--- modules/people/manifests/calorie.pp
+++ modules/people/manifests/calorie.pp
@@ -28,10 +28,16 @@ class people::calorie {
       'luajit',
       'reattach-to-user-namespace',
       'the_silver_searcher',
-      'macvim',
       'rmtrash',
       'tig',
     ]:
+  }
+  package { 'macvim':
+    ensure => installed,
+    install_options => [
+      '--with-cscope',
+      '--with-lua',
+    ];
   }
 
   $home     = "/Users/${::luser}"
@@ -40,12 +46,43 @@ class people::calorie {
   $ruby_env = {
     'RUBY_CONFIGURE_OPTS' => "--with-openssl-dir=${boxen::config::homebrewdir}/opt/openssl --enable-shared --with-readline-dir=${boxen::config::homebrewdir}/opt/readline"
   }
+  $ruby_version = '2.0.0-p353'
+  $ruby_version_link = '2.0.0'
 
-  ruby::version { '2.0.0-p247':
+  # install ruby
+  ruby::version { $ruby_version:
     env => $ruby_env
   }
+  file { "${boxen::config::home}/rbenv/versions/${ruby_version_link}":
+    ensure  => symlink,
+    force   => true,
+    target  => "${boxen::config::home}/rbenv/versions/${ruby_version}"
+  }
 
-  # class { 'ruby::global': version => '2.0.0-p247' }
+  # rbenv global
+  exec { "rbenv global ${ruby_version} && rbenv rehash": }
+  # class { 'ruby::global': version => $ruby_version_link }
+
+  # default gems
+  define default_gems ($gem = $title, $version) {
+    ruby::gem { "${gem} for ${version}":
+      gem  => $gem,
+      ruby => $version,
+    }
+  }
+  default_gems {
+    [
+      'bundler',
+      'rake',
+      'rubygems-update',
+      'rbenv-rehash',
+      'pry',
+      'tmuxinator',
+      'vvm-rb',
+    ]:
+    version => $ruby_version_link
+  }
+
   class { 'nodejs::global': version => 'v0.10.21' }
 
   Git::Config::Global <| title == "core.excludesfile" |> {

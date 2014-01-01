ccc27e816170ec806eff7e628b28721b8113db72
YuuShigetani <s2g4t1n2@gmail.com>
2014-01-02 02:45:28 +0900

    add ruby 2.1.0

--- modules/people/manifests/calorie/ruby.pp
+++ modules/people/manifests/calorie/ruby.pp
@@ -1,15 +1,15 @@
 class people::calorie::ruby {
   require openssl
-
   package { ['readline']: }
 
-  $rubies = ['1.9.3-p484', '2.0.0-p353']
-  $global_version = '2.0.0-p353'
+  $rubies = ['1.9.3-p484', '2.0.0-p353', '2.1.0']
+  $global_version = '2.1.0'
 
   # install ruby
   define install_rubies ($version = $title) {
     $ruby_configure_opts = "--enable-shared --with-openssl-dir=${boxen::config::homebrewdir}/opt/openssl --with-readline-dir=${boxen::config::homebrewdir}/opt/readline"
     $ruby_env = {
+      'LDFLAGS'             => '-L/opt/X11/lib',
       'RUBY_CONFIGURE_OPTS' => $ruby_configure_opts,
       'CONFIGURE_OPTS'      => $ruby_configure_opts,
     }
@@ -46,26 +46,4 @@ class people::calorie::ruby {
     mode    => '0644',
     content => "${global_version}\n",
   }
-
-  # vvm-rb
-  exec { 'vvm-rb for system':
-    command  => "env -i zsh -c 'source ${boxen::config::home}/env.sh && RBENV_VERSION=system gem install vvm-rb'",
-    creates  => "/Users/${::luser}/.vvm-rb",
-    provider => 'shell',
-    user     => 'root',
-    require  => [ Package['zsh'] ]
-  }
-  exec { 'install latest vim':
-    command  => "env -i zsh -c 'source ${boxen::config::home}/env.sh && RBENV_VERSION=system vvm-rb install --use latest --enable-multibyte --with-features=huge --enable-fontset --disable-selinux --enable-pythoninterp --enable-rubyinterp --enable-xim --enable-luainterp --with-lua-prefix=${boxen::config::homebrewdir}'",
-    creates  => "/Users/${::luser}/.vvm-rb",
-    provider => 'shell',
-    require  => [ Exec['vvm-rb for system'], Package['zsh'] ]
-  }
-  # exec { 'update vvm-rb':
-  #   command  => "env -i zsh -c 'source ${boxen::config::home}/env.sh && RBENV_VERSION=system gem update vvm-rb && RBENV_VERSION=system gem cleanup'",
-  #   onlyif   => "test -d /Users/${::luser}/.vvm-rb",
-  #   provider => 'shell',
-  #   user     => 'root',
-  #   require  => [ Package['zsh'] ]
-  # }
 }

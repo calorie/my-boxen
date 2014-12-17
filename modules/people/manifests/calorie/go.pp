5e878acfd462807a8626239a89c0636d082ff2a4
YuuShigetani <s2g4t1n2@gmail.com>
2014-12-17 21:09:32 +0900

    fix bugs

--- modules/people/manifests/calorie/go.pp
+++ modules/people/manifests/calorie/go.pp
@@ -1,14 +1,14 @@
 class people::calorie::go {
   include go
-
   $versions = ['1.1.2']
   $global_version = '1.1.2'
 
   go::version { $versions: }
 
   # global
-  file { "${boxen::config::home}/goenv/version":
+  file { "${boxen::config::home}/chgo/version":
     content => "${global_version}\n",
     replace => true,
+    require => [ Go::Version[$global_version] ],
   }
 }
--- modules/people/manifests/calorie/nodejs.pp
+++ modules/people/manifests/calorie/nodejs.pp
@@ -1,6 +1,10 @@
 class people::calorie::nodejs {
   $nodejs_global = 'v0.10'
 
+  unless defined(Nodejs::Version[$nodejs_global]) {
+    nodejs::version { $nodejs_global: }
+  }
+
   class { 'nodejs::global': version => $nodejs_global }
 
   $global_modules = ['coffee-script']

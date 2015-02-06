37514aff8c539d26810dac49f4e16a5a2f899d0b
YuuShigetani <s2g4t1n2@gmail.com>
2015-02-07 03:43:22 +0900

    remove ruby 2.1.5

--- modules/people/manifests/calorie/ruby.pp
+++ modules/people/manifests/calorie/ruby.pp
@@ -31,7 +31,7 @@ class people::calorie::ruby(
 
   # init refe2
   exec { 'init refe2 database':
-    command  => "env -i zsh -c '${gem_env} bitclust setup --versions=1.9.3,2.0.0,2.1.0'",
+    command  => "env -i zsh -c '${gem_env} bitclust setup --versions=1.9.3,2.2.0'",
     creates  => "/Users/${::boxen_user}/.bitclust",
     provider => 'shell',
     require  => [Exec["Install default-gems for ${global_version}"], Package['zsh']],
@@ -54,7 +54,7 @@ class people::calorie::ruby(
     require     => [Package['libxml2'], Package['libxslt']],
   }
   exec { 'bundle config nokogiri':
-    command     => "env -i zsh -c 'source ${boxen::config::home}/env.sh && RBENV_VERSION=${global_version} bundle config --global build.nokogiri --use-system-libraries --with-iconv-dir=$(brew --prefix libiconv) --with-xml2-dir=$(brew --prefix libxml2) --with-xslt-dir=$(brew --prefix libxslt)'",
+    command     => "env -i zsh -c '${gem_env} bundle config --global build.nokogiri --use-system-libraries --with-iconv-dir=$(brew --prefix libiconv) --with-xml2-dir=$(brew --prefix libxml2) --with-xslt-dir=$(brew --prefix libxslt)'",
     provider    => 'shell',
     subscribe   => [Package['libxml2'], Package['libxslt'], Package['libiconv']],
     refreshonly => true,

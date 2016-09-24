1e975308aea5ba627ed46ffbb9afd4ca63273415
YuuShigetani <s2g4t1n2@gmail.com>
2016-09-24 23:24:32 +0900

    fix mercurial

--- modules/people/manifests/calorie/mercurial.pp
+++ modules/people/manifests/calorie/mercurial.pp
@@ -1,5 +1,7 @@
 class people::calorie::mercurial {
   file { "/Users/${::boxen_user}/.hgrc":
+    ensure  => present,
+    replace => false,
     content => "[web]\ncacerts = ${boxen::config::homebrewdir}/etc/openssl/cert.pem",
     require => [Package['mercurial'], Package['openssl']]
   }

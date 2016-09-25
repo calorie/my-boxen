76cafcb578f7ed63a59d92e85ffd4f9ebe7e9141
YuuShigetani <s2g4t1n2@gmail.com>
2016-09-25 13:52:16 +0900

    fix bug

--- modules/people/manifests/calorie/ruby/gems.pp
+++ modules/people/manifests/calorie/ruby/gems.pp
@@ -2,7 +2,7 @@ define people::calorie::ruby::gems(
   $gem          = $title,
   $ruby_version = '*',
 ) {
-  ruby_gem { "${gem} for ${version}":
+  ruby_gem { "${gem} for ${ruby_version}":
     gem          => $gem,
     ruby_version => $ruby_version,
   }

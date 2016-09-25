c08fe05d5d0fe89ee125fccc309c195eb53fe5d6
YuuShigetani <s2g4t1n2@gmail.com>
2016-09-25 15:50:23 +0900

    revert

--- modules/people/manifests/calorie/ruby.pp
+++ modules/people/manifests/calorie/ruby.pp
@@ -13,23 +13,26 @@ class people::calorie::ruby(
   $rubies         = keys(hiera_hash('ruby::version::env', {}))
   $global_version = hiera('ruby::global::version', 'system')
   $vvm_rb_options = join($vvmopts, ' ')
+  $gem_env        = "source ${boxen::config::home}/env.sh && RBENV_VERSION=${global_version} rbenv exec"
 
   # install ruby
   people::calorie::ruby::versions { $rubies: }
 
   # init vvm-rb
   exec { 'init vvm-rb':
-    command  => "/opt/rubies/${global_version}/bin/vvm install latest ${vvm_rb_options}",
+    command  => "env -i SHELL=${boxen::config::homebrewdir}/bin/zsh zsh -c '${gem_env} vvm install latest ${vvm_rb_options}'",
+    provider => 'shell',
     creates  => "/Users/${::boxen_user}/.vvm-rb",
     timeout  => 1800,
-    require  => [Ruby_Gem["vvm-rb for ${global_version}"], Class['people::calorie::mercurial']],
+    require  => [Ruby::Version[$global_version], Ruby_Gem["vvm-rb for ${global_version}"], Package['zsh'], Class['people::calorie::mercurial']],
   }
 
   # init refe2
   exec { 'init refe2 database':
-    command  => "/opt/rubies/${global_version}/bin/bitclust setup --versions=2.3.0",
+    command  => "env -i SHELL=${boxen::config::homebrewdir}/bin/zsh zsh -c '${gem_env} bitclust setup --versions=2.3.0'",
+    provider => 'shell',
     creates  => "/Users/${::boxen_user}/.bitclust",
-    require  => [Ruby_Gem["refe2 for ${global_version}"]],
+    require  => [Ruby::Version[$global_version], Ruby_Gem["refe2 for ${global_version}"], Package['zsh']],
   }
 
   # nokogiri

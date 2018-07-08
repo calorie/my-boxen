e8cac726cb09500e7af001999244bae85b93ee84
YuuShigetani <s2g4t1n2@gmail.com>
2018-07-08 22:07:35 +0900

    fix

--- modules/people/manifests/calorie/python.pp
+++ modules/people/manifests/calorie/python.pp
@@ -1,9 +1,15 @@
 class people::calorie::python(
 ) {
+  exec { 'postinstall python3':
+    command     => 'brew postinstall python3',
+    subscribe   => [Package['python3']],
+    refreshonly => true,
+    require     => [Package['python3']],
+  }
   exec { 'pip3 install neovim':
     command     => 'pip3 install neovim',
-    subscribe   => [Package['neovim'], Package['python3']],
+    subscribe   => [Package['neovim'], Package['python3'], Exec['postinstall python3']],
     refreshonly => true,
-    require     => [Package['neovim'], Package['python3']],
+    require     => [Package['neovim'], Package['python3'], Exec['postinstall python3']],
   }
 }
